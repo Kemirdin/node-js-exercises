4feece809a75c2f4f854e69355c4c12cb6ceab1b
Kemirdin <qemirdin@gmail.com>
2018-03-15 18:20:18 +0100

    sync vs async changess

---
+++ understanding-node-core/5-readable_streams_events.js
@@ -0,0 +1,48 @@
+const fs = require ('fs');
+var stream = fs.createReadStream ('./input.txt');
+
+//non-flowing mode: stream pause is not possible
+stream.on ('readable', function () {
+  var chunk;
+  while ((chunk = stream.read ()) !== null) {
+    console.log (chunk);
+  }
+});
+
+stream.on ('end', function () {
+  console.log ('finished'); //no more data to read
+});
+
+stream.on ('close', function () {
+  console.log ('closed'); //file has been closed
+});
+
+stream.on ('error', function (err) {
+  console.log (err.stack);
+});
+
+/* Readable stream in flowing mode */
+
+var fs = require ('fs');
+var stream = fs.createReadStream ('./input.txt');
+
+stream.on ('data', function (chunk) {
+  console.log (chunk);
+  stream.pause ();
+  console.log ('Waiting for 1 second ...');
+  setTimeout (function () {
+    stream.resume ();
+  }, 1000);
+});
+
+stream.on ('end', function () {
+  console.log ('Finished!');
+});
+
+stream.on ('close', function () {
+  console.log ('closed!');
+});
+
+stream.on ('error', function (err) {
+  console.log (err.stack);
+});
